6565b0049bda5dc15998ef3a0c8d136077ce7398
KirillMikulich <kirill.mikulich@mail.ru>
2022-03-18 11:30:34 +0300

    add bus

--- TestMassTransit/Controllers/FirstController.cs
+++ TestMassTransit/Controllers/FirstController.cs
@@ -1,7 +1,6 @@
 ﻿using MassTransit;
 using MassTransit.Mediator;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
 using TestMassTransit.Consumers;
@@ -13,7 +12,7 @@ namespace TestMassTransit.Controllers
     public class FirstController : ControllerBase
     {
         private IBus _bus { get; set; }
-        private readonly IRequestClient<A> _requestClient; 
+        private readonly IRequestClient<A> _requestClient;
         private readonly IMediator _mediator;
         public FirstController(IBus bus, IRequestClient<A> requestClient, IMediator mediator)
         {
@@ -27,14 +26,35 @@ namespace TestMassTransit.Controllers
         {
             /*var client = _bus.CreateRequestClient<A>();
             var res = await client.GetResponse<B>(new A { id = 1 });*/
-            
-            
+
+
             //var response = await _requestClient.GetResponse<B>(new A { id = 1 });
+            try
+            {
+                var client = _mediator.CreateRequestClient<A>();
 
-            var client = _mediator.CreateRequestClient<A>();
+                var response = await client.GetResponse<B>(new A { id = 1 });
 
-            var response = await client.GetResponse<B>(new A { id = 1 });
-            return Ok(response.Message);
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            try
+            {
+                var client = _bus.CreateRequestClient<A>();
+                var res = await client.GetResponse<B>(new A { id = 1 });
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+
+
+            return Ok();
         }
     }
 }
